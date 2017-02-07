6273eafd089693da3ebbbc892de8a07535c4be3f
zachfedor <zachfedor@gmail.com>
2017-02-07 11:45:32 -0500

    adds styles and scripts to prevent page load transition bug

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // allows css transitions on all elements after initial load
+  var $body = document.querySelector('body');
+  $body.className = $body.className.replace('preload', '');
+
+});
